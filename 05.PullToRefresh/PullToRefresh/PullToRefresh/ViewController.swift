9b6cd11a3e5e25e008b87fbb038b3a5d8993fbfd
tbago <moonzalor@gmail.com>
2019-02-25 17:01:24 +0800

    Add 05.PullToRefresh project.

---
+++ 05.PullToRefresh/PullToRefresh/PullToRefresh/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  PullToRefresh
+//
+//  Created by tbago on 2019/2/25.
+//  Copyright © 2019年 tbago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let refreshControl = UIRefreshControl()
+    var dataSrouce = Array<Date>()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refreshControl.attributedTitle = NSAttributedString(string: "下拉刷新")
+        refreshControl.addTarget(self, action: #selector(pullToRefeshAction), for: UIControl.Event.valueChanged)
+        tableView.addSubview(refreshControl)
+    }
+
+    @objc func pullToRefeshAction() {
+        dataSrouce.insert(NSDate() as Date, at: 0)
+        refreshControl.endRefreshing()
+        tableView.reloadData()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSrouce.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableCell = tableView.dequeueReusableCell(withIdentifier: "PullToRefreshTableViewCell", for: indexPath)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy年MM月dd日/HH时mm分ss秒"
+        let formatString = dateFormatter.string(from: dataSrouce[indexPath.row]);
+        tableCell.textLabel?.text = formatString
+        
+        return tableCell
+    }
+}
+
